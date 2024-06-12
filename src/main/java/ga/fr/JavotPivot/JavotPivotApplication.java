37f993a4bbf064c14899eecc8e572998390f646f
YakaY91 <xavier.lscx@gmail.com>
2024-06-12 15:08:53 +0200

    first commit repo initialization

---
+++ src/main/java/ga/fr/JavotPivot/JavotPivotApplication.java
@@ -0,0 +1,13 @@
+package ga.fr.JavotPivot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavotPivotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavotPivotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ga/fr/JavotPivot/JavotPivotApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.fr.JavotPivot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavotPivotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
